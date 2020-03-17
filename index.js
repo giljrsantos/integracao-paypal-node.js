c5cc660e2b2b623236e720465cbec3eee23b69b6
Gilberto Junior <giljrsantos@gmail.com>
2020-03-16 21:58:15 -0300

    Primeira Versão Projeto Integração PayPal + Node.Js

---
+++ index.js
@@ -0,0 +1,298 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const paypal = require("paypal-rest-sdk");
+
+// View engine
+app.set('view engine', 'ejs');
+
+//Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Configuração de acesso ao PayPal
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': 'AQYgTpZOMXNGpc_WG89wYkY8TlIBCbQa0UgMpGm6A8L3W-8GXlyPahKIznrJYjnpc11DvKLIm41qaFJU',
+    'client_secret': 'EJ9Enu4MPk_ozo8n2BwHhZylUuJOpz2GWedjByZl14h1vSnm1Sv6RRFuyOWGgKkx7qfkm8bGpAdkmnub'
+});
+
+//Rota Principal
+app.get("/", (req, res) => {
+
+    res.render("index");
+
+});
+
+//Rota para Compra
+app.post("/comprar", (req, res) => {
+
+    var email = req.body.email;
+    var id = req.body.id;
+
+    var { name, price, amount } = req.body;
+    var total = price * amount;
+
+    var pagamento = {
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": `http://localhost:3300/final?email=${email}&id=${id}&total=${total}`,
+            "cancel_url": "http://cancel.url"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": name,
+                    "sku": name,
+                    "price": price,
+                    "currency": "BRL",
+                    "quantity": amount
+                }]
+            },
+            "amount": {
+                "currency": "BRL",
+                "total": total
+            },
+            "description": name
+        }]
+    };
+
+    paypal.payment.create(pagamento, (error, payment) => {
+        if (error) {
+            console.log(error);
+        } else {
+
+            for (var i = 0; i < payment.links.length; i++) {
+                var p = payment.links[i];
+                if (p.rel === 'approval_url') {
+                    res.redirect(p.href);
+                }
+            }
+
+        }
+    })
+
+
+
+});
+
+
+//Rota Final para concluir o pagamento
+app.get('/final', (req, res) => {
+
+
+    var payerId = req.query.PayerID;
+    var paymentId = req.query.paymentId;
+
+
+    //Dados do Cliente
+    var emailDoCliente = req.query.email;
+    var idDoCliente = req.query.id;
+
+    //Total da Compra   
+    var total = req.query.total
+
+
+    var final = {
+        "payer_id": payerId,
+        "transactions": [{
+            "amount": {
+                "currency": "BRL",
+                "total": total
+            }
+        }]
+    }
+
+    paypal.payment.execute(paymentId, final, (error, payment) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(payment);
+        }
+    });
+});
+
+//Rota para Criar o Plano de Pagamento
+app.get('/create', (req, res) => {
+
+    var plan = {
+        "name": "Plano Prata",
+        "description": "Um plano bem barato e muito bom!",
+        "merchant_preferences": {
+            "auto_bill_amount": "yes",
+            "cancel_url": "http://www.cancel.com",
+            "initial_fail_amount_action": "continue",
+            "max_fail_attempts": "1",
+            "return_url": "http://www.success.com",
+            "setup_fee": {
+                "currency": "BRL",
+                "value": "0"
+            }
+        },
+        "payment_definitions": [
+            {
+                "amount": {
+                    "currency": "BRL",
+                    "value": "0"
+                },
+                "cycles": "7",
+                "frequency": "DAY",
+                "frequency_interval": "1",
+                "name": "Teste Grátis",
+                "type": "TRIAL"
+            },
+            {
+                "amount": {
+                    "currency": "BRL",
+                    "value": "24"
+                },
+                "cycles": "0",
+                "frequency": "MONTH",
+                "frequency_interval": "1",
+                "name": "Regular Prata",
+                "type": "REGULAR"
+            }
+        ],
+        "type": "INFINITE"
+    }
+
+    paypal.billingPlan.create(plan, (err, plan) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(plan);
+            res.json(plan);
+        }
+    });
+
+});
+
+//Rota para listar os panos de pagamento que esta ativo
+app.get('/list', (req, res) => {
+
+    paypal.billingPlan.list({ 'status': 'ACTIVE' }, (error, plans) => {
+
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(plans);
+        }
+    });
+});
+
+app.get('/active/:id', (req, res) => {
+
+    var mudancas = [
+        {
+            "op": "replace",
+            "path": "/",
+            "value": {
+                "state": "ACTIVE"
+            }
+        }
+    ]
+
+    paypal.billingPlan.update(req.params.id, mudancas, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send("Mudança Feita!");
+        }
+
+    });
+});
+
+//Rota para submeter o cliente a assinatura
+app.post('/sub', (req, res) => {
+    var email = req.body.email;
+    var idDoPlano = "P-9D392689YU833910K4E36VUI";
+
+    //Transformando a data Atual em data no formato ISO
+    var isoDate = new Date(Date.now());
+    isoDate.setSeconds(isoDate.getSeconds() + 4);
+    isoDate.toISOString().slice(0, 19) + 'Z';
+
+    var dadosDaAssinatura = {
+        "name": "Assinatura do Plano Prata",
+        "description": "Plano prata R$ 25,00 por mês",
+        "start_date": isoDate,
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "plan": {
+            "id": idDoPlano
+        },
+        "override_merchant_preferences": {
+            "return_url": `http://localhost:3300/subreturn?email=${email}`,
+            "cancel_url": "https://exemple.com/cancel"
+        }
+    }
+
+    paypal.billingAgreement.create(dadosDaAssinatura, (error, assinatura) => {
+
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(assinatura);
+        }
+    });
+
+});
+
+
+//rota com o retorno da assinatura para o cliente
+app.get("/subreturn", (req, res) => {
+
+    var email = req.query.email;
+    var token = req.query.token;
+
+    paypal.billingAgreement.execute(token, {}, (error, assinatura) => {
+
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(assinatura);
+        }
+    });
+
+});
+
+//Rota para informação do plano
+app.get('/info/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    paypal.billingAgreement.get(id, (error, assinatura) => {
+
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(assinatura);
+        }
+    });
+});
+
+//Rota para Cancelamento do plano pelo cliente
+app.get('/cancel/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    paypal.billingAgreement.cancel(id, { "note": "O Cliente Pediu para cancelar!" }, (error, response) => {
+
+        if (error) {
+            console.log(error);
+        } else {
+            res.send("Assinatura cancelada!");
+        }
+    });
+});
+
+
+//Porta que vai o sistema vai rodar
+app.listen(3300, () => {
+    console.log("Running!")
+})
+
